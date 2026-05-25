4fc83905a16f87b048d5eaf01e5764ceac9ed2c1
Bruno Matos <brunomatosbarbosa@hotmail.com>
2026-05-25 15:56:53 -0300

    ajustes

--- src/main/java/br/gov/es/pmo/organization_parser/pmo_base/service/ProviderService.java
+++ src/main/java/br/gov/es/pmo/organization_parser/pmo_base/service/ProviderService.java
@@ -4,71 +4,33 @@
  */
 package br.gov.es.pmo.organization_parser.pmo_base.service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import br.gov.es.pmo.organization_parser.pmo_base.model.IOrganizationParser;
 import br.gov.es.pmo.organization_parser.pmo_base.model.OrganizationDto;
 
 
-/**
- *
- * @author gean.carneiro
- */
+
 @Service
 public class ProviderService {
-    
+
     private final Map<String, IOrganizationParser<?>> providers;
-    
-    private final ClientCredentialService clientCredentialService;
-        
-    public ProviderService(
-        final Map<String, IOrganizationParser<?>> providers,
-        final ClientCredentialService clientCredentialService
-    ){
+
+    public ProviderService(Map<String, IOrganizationParser<?>> providers) {
         this.providers = providers;
-        this.clientCredentialService = clientCredentialService;
     }
-    
-    private OAuth2AuthenticationToken getToken() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (auth instanceof OAuth2AuthenticationToken) {
-            return (OAuth2AuthenticationToken) auth;
+    public List<OrganizationDto> getOrganizations(String registrationId, String token) {
+
+        IOrganizationParser<?> provider = providers.get(registrationId);
+
+        if (provider == null) {
+            return Collections.emptyList();
         }
 
-        throw new IllegalStateException("Usuario não autenticado");
+        return provider.getOrganizations(token);
     }
-
-    private String getRegistrationId() {
-        return getToken().getAuthorizedClientRegistrationId();
-    }
-    
-    private String getClientToken() {
-        return clientCredentialService.getClientToken(this.getRegistrationId());
-    }
-       
-    
-    
-    public IOrganizationParser<?> getProvider() {
-        
-        IOrganizationParser<?> provider = this.providers.get(this.getRegistrationId());
-        
-        if(provider == null) {
-            throw new IllegalStateException("Provider não implementado: " + getRegistrationId() );
-        }
-
-        
-        return provider;
-                    
-    }
-    
-    public List<OrganizationDto> getOrganizations() {
-        return this.getProvider().getOrganizations(getClientToken());
-    }
-    
 }
